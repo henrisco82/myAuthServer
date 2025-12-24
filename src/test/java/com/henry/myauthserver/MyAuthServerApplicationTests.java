25e563a727106df5c2cc0d78bf2b967abb986a0d
Henry Odohi Unah <henry.unah@gmail.com>
2025-12-24 22:29:54 +0100

    first commit

---
+++ src/test/java/com/henry/myauthserver/MyAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.myauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
