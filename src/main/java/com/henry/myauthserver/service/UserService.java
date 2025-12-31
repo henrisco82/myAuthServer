f4eef0637d802f30e370ed1b50b2694902678689
Henry Odohi Unah <henry.unah@gmail.com>
2025-12-31 11:40:22 +0100

    Simplify database config - use separate env vars for username/password

--- src/main/java/com/henry/myauthserver/service/UserService.java
+++ src/main/java/com/henry/myauthserver/service/UserService.java
@@ -45,3 +45,5 @@ public class UserService {
         return userRepository.count();
     }
 }
+
+jdbc:postgresql://myauthserver_db_user:JLXNfztyd9q8lH524bhtT00gL8VT8guT@dpg-d59cjs3uibrs73b7rimg-a/myauthserver_db
